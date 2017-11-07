a6178188541f371de43d5afee72f88c2d0a13adf
BautiG <juanbautistagallino@hanovernorwichschools.org>
2017-11-07 12:06:47 -0500

    Updated from Brython Server: 07/11/2017 12:06:46

--- Calculus.py
+++ Calculus.py
@@ -1,7 +1,8 @@
-Function = input("What is your function?")
+"""Function = input("What is your function?")"""
 Interval1 = int(input("Where does your interval start?"))
 Interval2 = int(input("And where does it end?"))
 
+Function = x^2 +3
 for t in Function:
     #here we find the slope at every point
 
