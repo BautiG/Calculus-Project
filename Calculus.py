dba633c2f99a001c6d2032d91d523add60ddbbc4
BautiG <juanbautistagallino@hanovernorwichschools.org>
2017-11-07 12:10:04 -0500

    Updated from Brython Server: 07/11/2017 12:10:04

--- Calculus.py
+++ Calculus.py
@@ -1,6 +1,7 @@
 #Function = input("What is your function?")
 Interval1 = int(input("Where does your interval start?"))
 Interval2 = int(input("And where does it end?"))
+Accuracy = input("How accurate do you want the graph to be?")
 
 Function = x^2 +3
 for t in Function:
