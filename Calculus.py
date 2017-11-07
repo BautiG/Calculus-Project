5b9f0328784400195367fd40fa842a3c91041a56
BautiG <juanbautistagallino@hanovernorwichschools.org>
2017-11-07 16:21:57 -0500

    Updated from Brython Server: 07/11/2017 16:21:56

--- Calculus.py
+++ Calculus.py
@@ -4,7 +4,6 @@ Interval2 = int(input("And where does it end?"))
 #Accuracy = input("How accurate do you want the graph to be?")
 
 
-
 for x in range(Interval1, Interval2+1):
             print(x)
 
