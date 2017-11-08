6d6d58cc548a1c5842aadab7ec6b05f3686100ae
BautiG <juanbautistagallino@hanovernorwichschools.org>
2017-11-08 11:29:30 -0500

    Updated from Brython Server: 08/11/2017 11:29:29

--- Calculus.py
+++ Calculus.py
@@ -1,15 +1,10 @@
 import math
-"""
-Function = input("What is your function?")
-Interval1 = int(input("Where does your interval start?"))
-Interval2 = int(input("And where does it end?"))
+yFunction = str(input("y="))
+xValue = int(input("Where does your interval start?"))
+xValue2 = int(input("And where does it end?"))
 #Accuracy = input("How accurate do you want the graph to be?")
-"""
+
 def funcTion():
-    yFunction = str(input("y="))
-    print (yFunction)
-    xInput = input("x=")
-    xValue = int(xInput)
     yFunctionNew = ""
     
     for i in range(len(yFunction)):
@@ -17,10 +12,12 @@ def funcTion():
             yFunctionNew += str(xValue)
         else:
             yFunctionNew += yFunction[i]
-            
+
     i += 1
 
     print (yFunctionNew)
     print (eval(yFunctionNew))
-
-funcTion()
+    
+for i in range(xValue, xValue2):
+    funcTion()
+    xValue+=1
