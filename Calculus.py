c7808c91adcdc4264da1bf049d4ef9e673416a43
BautiG <juanbautistagallino@hanovernorwichschools.org>
2017-11-13 11:44:52 -0500

    Update Calculus.py

--- Calculus.py
+++ Calculus.py
@@ -4,7 +4,7 @@ import math
 yFunction = str(input("y="))
 reSet = int(input("Where does your interval start? "))
 xValue2 = int(input("And where does it end? "))
-#Accuracy = input("How accurate do you want the graph to be?")
+accuracy = int(input("Between integers, how many times should the program check for slope? "))
 
 xValue=reSet
 
@@ -19,12 +19,11 @@ def funcTion():
     i += 1
     
     yFunctionNew=eval(yFunctionNew)
-    #print (yFunctionNew)
-    #print (eval(yFunctionNew))
+    print (yFunctionNew)
 
-for i in range(xValue, xValue2+1):
+for i in range(xValue*accuracy, ((xValue2)*accuracy)+1):
     funcTion()
-    xValue+=1
+    xValue+=(1/accuracy)
 """
 maxXvalue = 0  #this just establishes a veriable
 xValue=reSet
@@ -37,7 +36,7 @@ for i in range(xValue, xValue2+1):
         maxXvalue = xValue  #this step makes that variable equal to the xvalue of the surent highest point
     else:
         biggest=biggest
-    xValue+=1
+    xValue+=(1/accuracy)
 
 minXvalue = 0
 xValue=reSet
@@ -48,7 +47,7 @@ for i in range(xValue, xValue2+1):
         minXvalue = xValue
     else:
         smallest=smallest
-    xValue+=1
+    xValue+=(1/accuracy)
 
 print("Absolute Max at: y= " + str(maxXvalue) + "," + str(biggest)) 
 print("Absolute Min at: y= " + str(minXvalue) + "," + str(smallest))
@@ -78,10 +77,48 @@ def nDer():
     negativePoint=eval(yFunctionNewDer2)
     
     derSlope=(positivePoint-negativePoint)/.002
-    print(derSlope)
+    #print(derSlope)
+
+def nDer2():
+    yFunctionNewDer3 = ""
+    
+    for i in range(len(yFunction)):
+        if yFunction[i] == "x":
+            yFunctionNewDer3 += str(xValue+1+.001)
+        else:
+            yFunctionNewDer3 += yFunction[i]
+    i += 1
+    
+    positivePoint2=eval(yFunctionNewDer3)
+    
+    yFunctionNewDer4 = ""
+    
+    for i in range(len(yFunction)):
+        if yFunction[i] == "x":
+            yFunctionNewDer4 += str(xValue+1-.001)
+        else:
+            yFunctionNewDer4 += yFunction[i]
+    i += 1
+    
+    negativePoint2=eval(yFunctionNewDer4)
+    
+    derSlope2=(positivePoint2-negativePoint2)/.002
+    #print(derSlope2)
+    
+
+"""
+for i in range(xValue, xValue2+1):
+    nDer()
+    xValue+=(1/accuracy)
 
 for i in range(xValue, xValue2+1):
     nDer()
+    nDer2()
+    if derSlope>0 and derSlope2<0:
+        print("max")
+    if derSlope<0 and derSlope2>0:
+        print("min")
+    else:
+        print("what the herrrr")
     xValue+=1
-
-
+"""
